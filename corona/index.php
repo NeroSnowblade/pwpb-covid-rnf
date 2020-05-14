984885c9961a2732afb65ccc68fbb3edb8e2d4d3
najieb25 <61814248+najieb25@users.noreply.github.com>
2020-05-14 08:29:52 +0700

    Add files via upload

---
+++ corona/index.php
@@ -0,0 +1,226 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+     <style type="text/css">
+   .left    { text-align: left;}
+   .right   { text-align: right;}
+   .center  { text-align: center;}
+   .justify { text-align: justify;}
+</style>
+
+     <title>Corona virus</title>
+<!--
+
+Template 2098 Health
+
+http://www.tooplate.com/view/2098-health
+
+-->
+     <meta charset="UTF-8">
+     <meta http-equiv="X-UA-Compatible" content="IE=Edge">
+     <meta name="description" content="">
+     <meta name="keywords" content="">
+     <meta name="author" content="Tooplate">
+     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+
+     <link rel="stylesheet" href="css/bootstrap.min.css">
+     <link rel="stylesheet" href="css/font-awesome.min.css">
+     <link rel="stylesheet" href="css/animate.css">
+     <link rel="stylesheet" href="css/owl.carousel.css">
+     <link rel="stylesheet" href="css/owl.theme.default.min.css">
+
+     <!-- MAIN CSS -->
+     <link rel="stylesheet" href="css/tooplate-style.css">
+
+</head>
+<body id="top" data-spy="scroll" data-target=".navbar-collapse" data-offset="50">
+
+     <!-- PRE LOADER -->
+     <section class="preloader">
+          <div class="spinner">
+
+               <span class="spinner-rotate"></span>
+               
+          </div>
+     </section>
+
+
+     <!-- HEADER -->
+     <header>
+          <div class="container">
+               <div class="row">
+
+                    <div class="col-md-4 col-sm-5">
+                         
+                    </div>
+                         
+                    <div class="col-md-8 col-sm-7 text-align-right">
+                    </div>
+
+               </div>
+          </div>
+     </header>
+
+
+     <!-- MENU -->
+    
+
+                    <!-- lOGO TEXT HERE -->
+                    <a href="index.php" class="center"></i><b><h3>CORONA VIRUS</h3></b></a>
+               </div>
+
+               <!-- MENU LINKS -->
+                         <br>
+                         <br>
+                         <br>
+                         <br>
+
+                         <p align="center"><img src="home.JPG"  height="75%" width="75%"/></p>
+                         
+                    </ul>
+
+
+                        
+                    </ul>
+               </div>
+
+          </div>
+     </section>
+
+
+     <!-- HOME -->
+               <div class="row">
+     <section id="home" class="slider" data-stellar-background-ratio="0.5">
+          <div class="container">
+
+                         
+
+
+     <!-- TEAM -->
+     
+                         </div>
+                    </div>
+                    
+               </div>
+          </div>
+     </section>
+
+
+     <!-- NEWS -->
+     <br>
+     <b> <h4 class="center">Pengertian Coronavirus</b></h4>
+          <Br>
+                <p class="left">Coronavirus atau virus corona merupakan keluarga besar virus yang menyebabkan infeksi saluran pernapasan atas ringan hingga sedang, seperti penyakit flu. Banyak orang terinfeksi virus ini, setidaknya satu kali dalam hidupnya.</p>
+
+                <p class="left" > Namun, beberapa jenis virus corona juga bisa menimbulkan penyakit yang lebih serius, seperti:</p>
+
+          <br>
+               <ul type="circle">
+                    <li>Middle East Respiratory Syndrome (MERS-CoV).</li> 
+                    <li>Severe Acute Respiratory Syndrome (SARS-CoV).</li>
+                    <li>Pneumonia.</li>
+               </ul>
+          <br> 
+               <p class="left">Sampai saat ini terdapat tujuh coronavirus (HCoVs) yang telah diidentifikasi, yaitu:</p>
+               <ul type="circle">
+                    HCoV-229E.</li>
+                <li>HCoV-OC43.</li>
+                <li>HCoV-NL63.</li>
+                <li>HCoV-HKU1.</li>
+                <li>SARS-COV (yang menyebabkan sindrom pernapasan akut).</li>
+                <li>MERS-COV (sindrom pernapasan Timur Tengah).</li>
+                <li>COVID-19 atau dikenal juga dengan Novel Coronavirus (menyebabkan wabah pneumonia di kota Wuhan, Tiongkok pada Desember 2019, dan menyebar ke negara lainnya mulai Januari 2020. Indonesia sendiri mengumumkan adanya kasus covid 19 dari Maret 2020</li>
+               </ul>
+
+          <br>
+               <b><h4 class="center">Faktor Risiko Infeksi Coronavirus</h4></b></Br>  
+
+                <p> Siapa pun dapat terinfeksi virus corona. Akan tetapi, bayi dan anak kecil, serta orang dengan kekebalan tubuh yang lemah lebih rentan terhadap serangan virus ini. Selain itu, kondisi musim juga mungkin berpengaruh. Contohnya, di Amerika Serikat, infeksi virus corona lebih umum terjadi pada musim gugur dan musim dingin.</p> 
+          <br>
+               <p >Di samping itu, seseorang yang tinggal atau berkunjung ke daerah atau negara yang rawan virus corona, juga berisiko terserang penyakit ini. Misalnya, berkunjung ke Tiongkok, khususnya kota Wuhan, yang pernah menjadi wabah COVID-19 yang bermulai pada Desember 2019.</p>
+
+
+               <b><h4 class="center">Penyebab Infeksi Coronavirus</h4></b></Br>   
+         <p>Infeksi coronavirus disebabkan oleh virus corona itu sendiri. Kebanyakan virus corona menyebar seperti virus lain pada umumnya, seperti:</p>
+ <ul type="circle">
+               <li>Percikan air liur pengidap (bantuk dan bersin).</li>
+               <li>Menyentuh tangan atau wajah orang yang terinfeksi.</li>
+               <li>Menyentuh mata, hidung, atau mulut setelah memegang barang yang terkena percikan air liur pengidap virus corona.</li> 
+               <li>Tinja atau feses (jarang terjadi)</li>
+          <br>
+               <p>Khusus untuk COVID-19, masa inkubasi belum diketahui secara pasti. Namun, rata-rata gejala yang timbul setelah 2-14 hari setelah virus pertama masuk ke dalam tubuh. Di samping itu, metode transmisi COVID-19 juga belum diketahui dengan pasti. Awalnya, virus corona jenis COVID-19 diduga bersumber dari hewan. Virus corona COVID-19 merupakan virus yang beredar pada beberapa hewan, termasuk unta, kucing, dan kelelawar.</p> 
+
+Sebenarnya virus ini jarang sekali berevolusi dan menginfeksi manusia dan menyebar ke individu lainnya. Namun, kasus di Tiongkok kini menjadi bukti nyata kalau virus ini bisa menyebar dari hewan ke manusia. Bahkan, kini penularannya bisa dari manusia ke manusia. 
+
+           <br><b><h4 class="center">Gejala Infeksi Coronavirus</h4></b>
+     <br><p>Virus corona bisa menimbulkan beragam gejala pada pengidapnya. Gejala yang muncul ini bergantung pada jenis virus corona yang menyerang, dan seberapa serius infeksi yang terjadi. Berikut beberapa gejala virus corona yang terbilang ringan:</p>
+
+     <ul type="circle">     
+          <li>Hidung beringus.</li>
+          <li>Sakit kepala.</li>
+          <li>Batuk.</li>
+          <li>Sakit tenggorokan.</li>
+          <li>Demam.</li>
+          <li>Merasa tidak enak badan.</li>
+     </ul>
+     <br>
+
+          <p>Hal yang perlu ditegaskan, beberapa virus corona dapat menyebabkan gejala yang parah. Infeksinya dapat berubah menjadi bronkitis dan pneumonia (disebabkan oleh COVID-19), yang mengakibatkan gejala seperti:</p>
+
+          <ul type="circle">
+          <li>Demam yang mungkin cukup tinggi bila pasien mengidap pneumonia.</li>
+          <li>Batuk dengan lendir.</li>
+          <li>Sesak napas.</li>
+          <li>Nyeri dada atau sesak saat bernapas dan batuk.</li>
+          <li>Infeksi bisa semakin parah bila menyerang kelompok individu tertentu. Contohnya, orang dengan penyakit jantung atau paru-paru, orang dengan sistem kekebalan yang lemah, bayi, dan lansia. </li>
+          </ul> 
+
+          <br><b><h4 class="center">Berikut Informasi Terbaru Kasus Covid-19 Indonesia</h4></b></ul>  
+
+          <br>Selain itu, kamu juga bisa perkuat sistem kekebalan tubuh dengan konsumsi vitamin dan suplemen sebagai bentuk pencegahan dari virus ini.</br>
+
+
+
+     
+                    <div class="col-md-4 col-sm-6">
+                         <!-- NEWS THUMB -->
+                              
+                                      
+
+                    <div class="col-md-4 col-sm-6">
+                         <!-- NEWS THUMB -->
+                         
+                                        </div>
+                                   </div>
+                              </div>
+                         </div>
+                    </div>
+
+               </div>
+          </div>
+     </section>
+
+
+     <!-- MAKE AN APPOINTMENT -->
+     
+                                  
+
+                                   
+                    
+
+
+     <!-- GOOGLE MAP -->
+  
+
+     <!-- SCRIPTS -->
+     <script src="js/jquery.js"></script>
+     <script src="js/bootstrap.min.js"></script>
+     <script src="js/jquery.sticky.js"></script>
+     <script src="js/jquery.stellar.min.js"></script>
+     <script src="js/wow.min.js"></script>
+     <script src="js/smoothscroll.js"></script>
+     <script src="js/owl.carousel.min.js"></script>
+     <script src="js/custom.js"></script>
+
+</body>
+</html>
